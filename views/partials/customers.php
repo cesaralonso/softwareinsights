744b9a02bf2546c2fdee856941b4ddbf13aea88e
Paul Jimenez <pauljimenez@MacBook-Pro-de-Paul.local>
2017-08-29 14:47:46 -0500

    customers

--- views/partials/customers.php
+++ views/partials/customers.php
@@ -7,5 +7,8 @@
     <div class="col-sm-2">
       <img src="<?php echo APP_PATH; ?>images/BIHAIV.png" alt="" class="customers-img">
     </div>
+    <div class="col-sm-2">
+      <img src="<?php echo APP_PATH; ?>images/LAVILLABELLA.png" alt="" class="customers-img">
+    </div>
   </div>
 </div>

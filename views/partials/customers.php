643df04123d450749b4a3371a568ad5946dfd1eb
Paul Jimenez <pauljimenez@MacBook-Pro-de-Paul.local>
2017-08-29 14:44:34 -0500

    customers

--- views/partials/customers.php
+++ views/partials/customers.php
@@ -1,11 +1,11 @@
 <div class="container-fluid">
   <div class="row">
     <h2 class="text-center">Nuestros Clientes</h2>
-    <div class="col-sm-6">
+    <div class="col-sm-2">
       <img src="<?php echo APP_PATH; ?>images/icode.png" alt="" class="customers-img">
     </div>
-    <div class="col-sm-6">
-      <img src="<?php echo APP_PATH; ?>images/icode.png" alt="" class="customers-img">
+    <div class="col-sm-2">
+      <img src="<?php echo APP_PATH; ?>images/BIHAIV.png" alt="" class="customers-img">
     </div>
   </div>
 </div>

6f4dffaf330a52ec95d4732d4d4b805143ef78e4
cesardesarrollo <cesar_alonso_m_g@hotmail.com>
2017-08-29 15:08:22 -0500

    Menú main corregido enlaces

--- views/partials/nav-main.php
+++ views/partials/nav-main.php
@@ -6,15 +6,15 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#top"><img src="<?php echo APP_PATH; ?>images/Imagotipo.png" class="logo-nav" alt=""></a>
+      <a class="navbar-brand" href="<?php echo (($view === 'oferta-laboral-y-becarios') ? APP_PATH.'index.php#top' : '#top'); ?>"><img src="<?php echo APP_PATH; ?>images/Imagotipo.png" class="logo-nav" alt=""></a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="<?php echo (($view === 'oferta-laboral-y-becarios') ? APP_PATH.'#about' : '#about'); ?>">NOSOTROS</a></li>
-        <li><a href="<?php echo (($view === 'oferta-laboral-y-becarios') ? APP_PATH.'#services' : '#services'); ?>">SERVICIOS</a></li>
-        <li><a href="<?php echo (($view === 'oferta-laboral-y-becarios') ? APP_PATH.'#ubicacion' : '#ubicacion'); ?>">UBICACIÓN</a></li>
+        <li><a href="<?php echo (($view === 'oferta-laboral-y-becarios') ? APP_PATH.'index.php#about' : '#about'); ?>">NOSOTROS</a></li>
+        <li><a href="<?php echo (($view === 'oferta-laboral-y-becarios') ? APP_PATH.'index.php#services' : '#services'); ?>">SERVICIOS</a></li>
+        <li><a href="<?php echo (($view === 'oferta-laboral-y-becarios') ? APP_PATH.'index.php#ubicacion' : '#ubicacion'); ?>">UBICACIÓN</a></li>
         <li <?php echo (($view === "oferta-laboral-y-becarios") ? "class='active'" : ''); ?>><a href="<?php echo APP_PATH.'servicios/oferta-laboral-y-becarios'; ?>">VACANTES</a></li>
-        <li><a href="<?php echo (($view === 'oferta-laboral-y-becarios') ? APP_PATH.'#contact' : '#contact'); ?>">CONTACTO</a></li>
+        <li><a href="<?php echo (($view === 'oferta-laboral-y-becarios') ? APP_PATH.'index.php#contact' : '#contact'); ?>">CONTACTO</a></li>
       </ul>
     </div>
   </div>

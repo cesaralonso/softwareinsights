c65ce844dfcf3b5a2476cc3c4a3b3b406d1c1ea2
Paul Jimenez <pauljimenez@MacBook-Pro-de-Paul.local>
2017-08-29 14:54:56 -0500

    oferta laboral

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
   ga('send', 'pageview');
 </script>
 
-<?php include("views/partials/".(($view === 'home') ? 'nav-main.php' : 'nav-pages.php')); ?>
+<?php include("views/partials/".(($view === 'home' || $view === 'oferta-laboral-y-becarios') ? 'nav-main.php' : 'nav-pages.php')); ?>
 
 <?php include("views/$view.php"); ?>
 
